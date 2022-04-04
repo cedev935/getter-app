30a5ba350f4f9f1eb67bd5be9394143413e28a55
DavidBartTobi <82588072+DavidBartTobi@users.noreply.github.com>
2022-04-05 01:52:50 +0300

    Delete app directory

--- app/components/lists/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ListGoal } from "./ListGoal";
-export { default as ListItemDeleteAction } from "./ListItemDeleteAction";
-export { default as ListItemSeparator } from "./ListItemSeparator";
